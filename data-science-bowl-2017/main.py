5ef338d3d2b3c0551bd2e87a56eb72af27a39a33
Jovan Sardinha <jovan.sardinha@gmail.com>
2017-01-15 19:26:21 -0500

    udpated main

--- data-science-bowl-2017/main.py
+++ data-science-bowl-2017/main.py
@@ -1,1 +1,30 @@
 # main
+
+## Local Modules
+​
+​
+## Global Modules
+
+
+# Constants
+data_url = "../../data-science-bowl-2017-data/"
+labels_data = 'stage1_labels.csv'
+train_data = 'stage1/'
+
+
+# Pre-processing
+​
+​
+# Model Building & Traning
+
+
+# Predicting on CV
+
+
+# Preducting on Test
+
+
+# Post-test Analysis
+
+
+# Submission
