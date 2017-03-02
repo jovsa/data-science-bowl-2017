d7716e9c019a0a1c45c275f7d882f07a648dbe99
JovanSarinha <jovan.sardinha@gmail.com>
2017-03-02 04:37:44 +0000

    initial implimentation for train_nn()

--- data-science-bowl-2017/main.py
+++ data-science-bowl-2017/main.py
@@ -31,6 +31,7 @@ from sklearn.decomposition import PCA
 from time import time
 
 import tensorflow as tf
+import prettytensor as pt
 
 ######################
 def pre_process():
@@ -152,8 +153,202 @@ def train_xgboost():
     clf.fit(trn_x, trn_y, eval_set=[(val_x, val_y)], verbose=True, eval_metric='logloss', early_stopping_rounds=50)
     return clf
 
-def make_submit():
-    clf = train_xgboost()
+
+# Start of transfer learning code
+def random_batch():
+    # Number of images (transfer-values) in the training-set.
+    num_images = len(transfer_values_train)
+
+    # Create a random index.
+    idx = np.random.choice(num_images,
+                           size=train_batch_size,
+                           replace=False)
+
+    # Use the random index to select random x and y-values.
+    # We use the transfer-values instead of images as x-values.
+    x_batch = transfer_values_train[idx]
+    y_batch = labels_train[idx]
+
+    return x_batch, y_batch
+
+def optimize(num_iterations):
+    # Start-time used for printing time-usage below.
+    start_time = time.time()
+
+    for i in range(num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images (transfer-values) and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = random_batch()
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the TensorFlow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        # We also want to retrieve the global_step counter.
+        i_global, _ = session.run([global_step, optimizer],
+                                  feed_dict=feed_dict_train)
+
+        # Print status to screen every 100 iterations (and last).
+        if (i_global % 100 == 0) or (i == num_iterations - 1):
+            # Calculate the accuracy on the training-batch.
+            batch_acc = session.run(accuracy,
+                                    feed_dict=feed_dict_train)
+
+            # Print status.
+            msg = "Global Step: {0:>6}, Training Batch Accuracy: {1:>6.1%}"
+            print(msg.format(i_global, batch_acc))
+
+    # Ending time.
+    end_time = time.time()
+
+    # Difference between start and end-times.
+    time_dif = end_time - start_time
+
+    # Print the time-usage.
+    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+
+def plot_example_errors(cls_pred, correct):
+    # This function is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # correct is a boolean array whether the predicted class
+    # is equal to the true class for each image in the test-set.
+
+    # Negate the boolean array.
+    incorrect = (correct == False)
+
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images = images_test[incorrect]
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = cls_test[incorrect]
+
+    n = min(9, len(images))
+
+    # Plot the first n images.
+    plot_images(images=images[0:n],
+                cls_true=cls_true[0:n],
+                cls_pred=cls_pred[0:n])
+
+# Import a function from sklearn to calculate the confusion-matrix.
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(cls_pred):
+    # This is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_test,  # True class for test-set.
+                          y_pred=cls_pred)  # Predicted class.
+
+    # Print the confusion matrix as text.
+    for i in range(num_classes):
+        # Append the class-name to each line.
+        class_name = "({}) {}".format(i, class_names[i])
+        print(cm[i, :], class_name)
+
+    # Print the class-numbers for easy reference.
+    class_numbers = [" ({0})".format(i) for i in range(num_classes)]
+    print("".join(class_numbers))
+
+# Split the data-set in batches of this size to limit RAM usage.
+batch_size = 256
+
+def predict_cls(transfer_values, labels, cls_true):
+    # Number of images.
+    num_images = len(transfer_values)
+
+    # Allocate an array for the predicted classes which
+    # will be calculated in batches and filled into this array.
+    cls_pred = np.zeros(shape=num_images, dtype=np.int)
+
+    # Now calculate the predicted classes for the batches.
+    # We will just iterate through all the batches.
+    # There might be a more clever and Pythonic way of doing this.
+
+    # The starting index for the next batch is denoted i.
+    i = 0
+
+    while i < num_images:
+        # The ending index for the next batch is denoted j.
+        j = min(i + batch_size, num_images)
+
+        # Create a feed-dict with the images and labels
+        # between index i and j.
+        feed_dict = {x: transfer_values[i:j],
+                     y_true: labels[i:j]}
+
+        # Calculate the predicted class using TensorFlow.
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+        # Set the start-index for the next batch to the
+        # end-index of the current batch.
+        i = j
+
+    # Create a boolean array whether each image is correctly classified.
+    correct = (cls_true == cls_pred)
+
+    return correct, cls_pred
+
+def predict_cls_test():
+    return predict_cls(transfer_values = transfer_values_test,
+                       labels = labels_test,
+                       cls_true = cls_test)
+
+def classification_accuracy(correct):
+    # When averaging a boolean array, False means 0 and True means 1.
+    # So we are calculating: number of True / len(correct) which is
+    # the same as the classification accuracy.
+
+    # Return the classification accuracy
+    # and the number of correct classifications.
+    return correct.mean(), correct.sum()
+
+def print_test_accuracy(show_example_errors=False,
+                        show_confusion_matrix=False):
+
+    # For all the images in the test-set,
+    # calculate the predicted classes and whether they are correct.
+    correct, cls_pred = predict_cls_test()
+
+    # Classification accuracy and the number of correct classifications.
+    acc, num_correct = classification_accuracy(correct)
+
+    # Number of images being classified.
+    num_images = len(correct)
+
+    # Print the accuracy.
+    msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+    print(msg.format(acc, num_correct, num_images))
+
+    # Plot some examples of mis-classifications, if desired.
+    if show_example_errors:
+        print("Example errors:")
+        plot_example_errors(cls_pred=cls_pred, correct=correct)
+
+    # Plot the confusion matrix, if desired.
+    if show_confusion_matrix:
+        print("Confusion Matrix:")
+        plot_confusion_matrix(cls_pred=cls_pred)
+
+
+
+
+def train_nn():
+    print('in train_nn')
 
     ids = list()
     for s in glob.glob(stage1_features_inception + "*"):
@@ -162,30 +357,108 @@ def make_submit():
         ids.append(id)
     ids = pd.DataFrame(ids,  columns=["id"])
 
-    submission_sample = pd.read_csv(stage1_submission)
-    df = pd.merge(submission_sample, ids, how='inner', on=['id'])
-    x = np.array([np.mean(np.load(stage1_features_inception + "inception_cifar10_" + s + ".pkl"), axis=0) for s in df['id'].tolist()])
+    df = pd.read_csv(labels)
+    df = pd.merge(df, ids, how='inner', on=['id'])
 
-    pred = clf.predict(x)
-    df['cancer'] = pred
 
-    #Submission preparation
-    submission = pd.merge(submission_sample, df, how='left', on=['id'])
-    submission = submission.iloc[:,(0,2)]
-    submission = submission.rename(index=str, columns={"cancer_y": "cancer"})
+    x = np.array([np.load(stage1_features_inception + "inception_cifar10_" + s + ".pkl") for s in df['id'].tolist()])
 
-    # Outputting submission file
-    timestamp = datetime.datetime.now().isoformat()
-    filename = submissions + 'submission-' + str(timestamp) + ".csv"
-    submission.to_csv(filename, index=False)
+    y = df['cancer'].as_matrix()
+    trn_x, val_x, trn_y, val_y = cross_validation.train_test_split(x, y, random_state=42, stratify=y,
+                                                                    test_size=0.20)
 
-    # Submission file analysis
-    print("----submission file analysis----")
-    patient_count = submission['id'].count()
-    predecited = submission['cancer'].count()
-    print("Total number of patients: " + str(patient_count))
-    print("Number of predictions: " + str(predecited))
-    print("submission file stored at: " + filename)
+    # flattening trn_x
+    for i in range(0, len(trn_x)):
+        print('original: ' +  str(trn_x[i].shape))
+        trn_x[i] = trn_x[i].flatten()
+        print('after: ' +  str(trn_x[i].shape))
+
+    # flattening val_x
+    for i in range(0, len(val_x)):
+        print('original: ' +  str(val_x[i].shape))
+        val_x[i] = val_x[i].flatten()
+        print('after: ' +  str(val_x[i].shape))
+
+    transfer_values_test = val_x
+    transfer_values_train = trn_x
+    labels_test = val_y
+    labels_train = trn_y
+    # cls_test
+    # cls_train
+
+    print("transfer_values_test : " + str(transfer_values_test.shape))
+    print("transfer_values_train : " + str(transfer_values_train.shape))
+    print("labels_test : " + str(labels_test.shape))
+    print("labels_train : " + str(labels_train.shape))
+    # print("cls_test : " + str(cls_test.shape))
+    # print("cls_train : " + str(cls_train.shape))
+
+    print('start of transfer learning tutorial')
+    model = inception.Inception()
+    transfer_len = model.transfer_len
+
+    x = tf.placeholder(tf.float32, shape=[None, transfer_len], name='x')
+    y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+    y_true_cls = tf.argmax(y_true, dimension=1)
+
+    x_pretty = pt.wrap(x)
+
+    with pt.defaults_scope(activation_fn=tf.nn.relu):
+        y_pred, loss = x_pretty.\
+            fully_connected(size=1024, name='layer_fc1').\
+            softmax_classifier(num_classes=num_classes, labels=y_true)
+
+    global_step = tf.Variable(initial_value=0,
+                          name='global_step', trainable=False)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step)
+    y_pred_cls = tf.argmax(y_pred, dimension=1)
+    correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    session = tf.Session()
+
+    session.run(tf.global_variables_initializer())
+
+    # print_test_accuracy(show_example_errors=False,
+    #                 show_confusion_matrix=False)
+
+    return
+
+def make_submit():
+    # clf = train_xgboost()
+    clf = train_nn()
+
+    # ids = list()
+    # for s in glob.glob(stage1_features_inception + "*"):
+    #     id = os.path.basename(s)
+    #     id = re.match(r'inception_cifar10_([a-f0-9].*).pkl' , id).group(1)
+    #     ids.append(id)
+    # ids = pd.DataFrame(ids,  columns=["id"])
+
+    # submission_sample = pd.read_csv(stage1_submission)
+    # df = pd.merge(submission_sample, ids, how='inner', on=['id'])
+    # x = np.array([np.mean(np.load(stage1_features_inception + "inception_cifar10_" + s + ".pkl"), axis=0) for s in df['id'].tolist()])
+
+    # pred = clf.predict(x)
+    # df['cancer'] = pred
+
+    # #Submission preparation
+    # submission = pd.merge(submission_sample, df, how='left', on=['id'])
+    # submission = submission.iloc[:,(0,2)]
+    # submission = submission.rename(index=str, columns={"cancer_y": "cancer"})
+
+    # # Outputting submission file
+    # timestamp = datetime.datetime.now().isoformat()
+    # filename = submissions + 'submission-' + str(timestamp) + ".csv"
+    # submission.to_csv(filename, index=False)
+
+    # # Submission file analysis
+    # print("----submission file analysis----")
+    # patient_count = submission['id'].count()
+    # predecited = submission['cancer'].count()
+    # print("Total number of patients: " + str(patient_count))
+    # print("Number of predictions: " + str(predecited))
+    # print("submission file stored at: " + filename)
 
 def file_exists(id):
     returnVal = True
@@ -295,6 +568,10 @@ if __name__ == '__main__':
 
     cifar10_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
     cifar_data = "/kaggle/dev/data-science-bowl-2017-data/CIFAR-10/"
+
+    ## nn hyper-params
+    num_classes = 2
+    train_batch_size = 64
 
 
     #process_pca()
