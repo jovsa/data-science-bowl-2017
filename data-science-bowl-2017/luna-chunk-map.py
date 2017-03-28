be199fc9fbebcafbd284cb03357c5f679f2e65e9
JovanSarinha <jovan.sardinha@gmail.com>
2017-03-28 02:59:18 +0000

    updated paths for luna-chunk-map

--- data-science-bowl-2017/luna-chunk-map.py
+++ data-science-bowl-2017/luna-chunk-map.py
@@ -20,8 +20,8 @@ from matplotlib import pyplot as plt
 import tensorflow as tf
 from tqdm import tqdm
 
-DATA_PATH_PREPROCESS = '/kaggle_2/luna/luna16/data/pre_processed_chunks_segmented_augmented_nz/'
-DATA_PATH_POSTPROCESS = '/kaggle_2/luna/luna16/data/pre_proc_chunks_seg_aug_nz_single/'
+DATA_PATH_PREPROCESS = '/kaggle_2/luna/luna16/data/pre_processed_chunks_augmented_v2_nz/'
+DATA_PATH_POSTPROCESS = '/kaggle_2/luna/luna16/data/pre_processed_chunks_augmented_v2_nz_single/'
 
 def shuffle_in_unison(arrays, random_seed=None):
     if random_seed:
@@ -37,30 +37,30 @@ def get_ids(PATH):
     for path in glob.glob(PATH + '[0-9\.]*_X.npy'):
         patient_id = re.match(r'([0-9\.]*)_X.npy', os.path.basename(path)).group(1)
         ids.append(patient_id)
-    return ids  
+    return ids
 
 def chunk_map():
     patient_ids = get_ids(DATA_PATH_PREPROCESS)
-    
+
     processed_patient_ids = set()
     patient_count = 0
     chunk_count = 0
-    
+
     for patient_id in patient_ids:
         if patient_id in processed_patient_ids:
             print('Skipping already processed patient {}'.format(patient_id))
             patient_count = patient_count + 1
             continue
-               
+
         x = np.load(DATA_PATH_PREPROCESS + patient_id + '_X.npy')
         y = np.load(DATA_PATH_PREPROCESS + patient_id + '_Y.npy')
-        
+
         print(patient_id + " " + str(x.shape[0]))
         for idx in range(x.shape[0]):
             chunk_id = str(uuid.uuid4())
             np.save(DATA_PATH_POSTPROCESS + chunk_id + '_X.npy', x[idx])
             np.save(DATA_PATH_POSTPROCESS + chunk_id + '_Y.npy', y[idx])
         chunk_count = chunk_count + x.shape[0]
-        
+
     print('Processed {} chunks'.format(str(chunk_count)))
 chunk_map()
