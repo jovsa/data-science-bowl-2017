767340a04a3b98dac2cb83c5920dbb23800565e5
JovanSarinha <jovan.sardinha@gmail.com>
2017-04-05 06:13:49 +0000

    run 15

--- data-science-bowl-2017/dsbowl-predict-cancer.py
+++ data-science-bowl-2017/dsbowl-predict-cancer.py
@@ -35,15 +35,15 @@ def get_patient_labels(patient_ids):
 def get_patient_features(patient_ids):
     input_features = {}
     MAX_CLASS_IDENTIFIER  = 2
-    NUM_BINS_3 = 10
-    NUM_BINS_2 = 10
-    NUM_BINS_1 = 10
-    NUM_BINS_0 = 10
+    NUM_BINS_3 = 100
+    NUM_BINS_2 = 100
+    NUM_BINS_1 = 100
+    NUM_BINS_0 = 100
 
-    TRESHOLD_3 = 0.20
-    TRESHOLD_2 = 0.20
-    TRESHOLD_1 = 0.20
-    TRESHOLD_0 = 0.20
+    TRESHOLD_3 = 0.00
+    TRESHOLD_2 = 0.00
+    TRESHOLD_1 = 0.00
+    TRESHOLD_0 = 0.00
 
 
     # import sys
